f96793b794054d563842b3936869400665535987
Anjali <anjalic@bsf.io>
2017-02-07 12:54:07 +0530

    attachment added to send_mail file

--- send_mail.php
+++ send_mail.php
@@ -14,17 +14,19 @@ $mail->Port = 587;
 $mail->setFrom('anjalic@bsf.io', 'Anjali Chavan',0);
 $mail->addReplyTo('anjalic@bsf.io', 'Anjali Chavan');
 $mail->addAddress('anjalic@bsf.io');  
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
 
-//$mail->isHTML(true);  
 
-// This is the simple email sent from localhost using PHP
+$mail->isHTML(true);  
 
-$bodyContent = 'This is the simple email sent from localhost using PHP';
 
+$bodyContent = 'This is the <b>simple email</b> sent from localhost using PHP ';
+$bodyContent='This file contains  <h3 style="color:red;">AUDI CAR</h3> image as attachment';
 $mail->Subject = 'Email from Localhost';
 $mail->Body    = $bodyContent;
+
+
+$mail->addAttachment('C:\Users\vrunda\Desktop\audi.png'); 
+
 
 if(!$mail->send()) {
     echo 'Message could not be sent.';
